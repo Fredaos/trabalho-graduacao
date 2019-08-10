050a9acf0e70e0883bf1b0d6e4260cd7b5193f04
Fredaos <frederico.aos@gmail.com>
2019-08-10 13:11:11 -0300

    adicionando arquivos de leitura do teste 6b

---
+++ serie-testes/teste6/graficos.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+dados = np.loadtxt("teste6a.txt")
+
+#fs = int(len(dados[:])/5)
+
+tempo = np.linspace(0,4,len(dados[:]))
+
+#grafico 2 adc
+plt.figure()
+plt.scatter(tempo,dados[:],c="red", marker = ".")
+plt.title("Tensão x Tempo ")
+plt.xlabel("tempo [s]")
+plt.ylabel("tensão [V]")
+plt.grid()
+plt.show()
+plt.savefig('teste6b_digipot.png')
